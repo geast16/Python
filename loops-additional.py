160847d78dad9ae014153784829fd5f12b7c4ba4
Greg <gregeast94@gmail.com>
2024-04-30 19:27:06 -0400

    additional loop content.

---
+++ loops-additional.py
@@ -0,0 +1,13 @@
+for i in range(11):
+    pass
+
+for a in range(1, 6):
+    for b in range(1, 6):
+        print(a, "x", b, "=", a * b)
+
+i = 2
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("else:", i)
