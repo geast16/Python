3a5706a6d556bf8fa761c17074ce0f100e9dab1a
Greg <gregeast94@gmail.com>
2024-04-30 23:03:10 -0400

    lists and methods.

---
+++ delete-lists.py
@@ -0,0 +1,13 @@
+top_cities = [
+    "New York City",
+    "Los Angeles",
+    "Singapore",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+]
+del top_cities[3:]
+print(top_cities)
+
+del top_cities
+print(top_cities)
